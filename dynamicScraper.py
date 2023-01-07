7d5cd20f95af843ccbaac4036f45dcc3012da655
Tautvydas <95254346+zarta21@users.noreply.github.com>
2023-01-07 14:33:39 +0100

    Update dynamicScraper.py

--- dynamicScraper.py
+++ dynamicScraper.py
@@ -2,62 +2,46 @@ from requests_html import HTMLSession
 import pandas as pd
 
 
-# You can find more information about requests_html on:
-# https://requests.readthedocs.io/projects/requests-html/en/latest/
-
-
 # url = 'https://negyvaseteris.lt/visos-prekes/' # url of the page from where we want get data
 
-s = HTMLSession() # start sesssion
-itemsList = [] # arr for products list
+s = HTMLSession() 
+itemsList = [] 
 
 def request(url):
-    # This funcion send request to the page and find our element:
+    r = s.get(url)
+    r.html.render(sleep = 1) 
 
-    r = s.get(url) # render the page
-
-    r.html.render(sleep = 1) # render page html (sleep is for pause)
-
-    return r.html.xpath('//*[@id="products-container"]/div/div/ul', first = True) # element that contains our data xpath
+    return r.html.xpath('//*[@id="products-container"]/div/div/ul', first = True) 
 
 
 def parse(products):
-    # Grab a list of all links on the page, in absolute form (anchors excluded) and loop for every product in the list:
 
     for item in products.absolute_links:
-        r = s.get(item) #rendering single product page (not list but only one product)
+        r = s.get(item) 
 
-        # Get desired data from the page:
-        # In this example, it will be the product title, price, and material
-        # In case there is no data, use try & except block to avoid stopping data extraction
         try:
-            title = r.html.find('h1.product_title', first = True).text # get product title
+            title = r.html.find('h1.product_title', first = True).text 
         except:
             title = "no title"
         try:
-            material = r.html.find('div.elementor-element.elementor-element-ad63ff6.elementor-widget.elementor-widget-woocommerce-product-content > div > p:nth-child(1)', first = True).text # get product material
+            material = r.html.find('div.elementor-element.elementor-element-ad63ff6.elementor-widget.elementor-widget-woocommerce-product-content > div > p:nth-child(1)', first = True).text
         except:
             material = "no material"
 
-        price = r.html.find('span.woocommerce-Price-amount.amount', first = True).text # get product price
+        price = r.html.find('span.woocommerce-Price-amount.amount', first = True).text
         
-        
-        # create object with scraped data:
         product = {
             "Title": title,
             "Price": price,
             "Material": material,
         }
         
-        itemsList.append(product) # put the product into the products list
-
+        itemsList.append(product) 
 
 def output():
-    # This funcion put our data into Pandas data frame and saved it to CSV file:
-
-    df = pd.DataFrame(itemsList) # put products into Pandas data frame 
-    df.to_csv('negyvasEteris.csv') #creating csv file (if index is not needed just add index=False)
-    print("Saved to CSV file") #print messagge that work is done
+    df = pd.DataFrame(itemsList)  
+    df.to_csv('negyvasEteris.csv') 
+    print("Saved to CSV file") 
 
 
 
@@ -66,19 +50,17 @@ def output():
 
 #if url has more than one page, comment out the last two lines (data = request(url) and parse(data)) and uncomment code below
 
-#Pagination:
-i = 1 # page number
+i = 1
 
 while True:
     try:
         data = request(f'https://negyvaseteris.lt/visos-prekes/page/{i}')
-        print(f'Getting items from page {i}') #messagge to known that is working
+        print(f'Getting items from page {i}')
         parse(data)
         print("Total: ", len(itemsList))
 
-        i = i + 1 #after every loop, increase the page number by 1
+        i = i + 1 
     except:
-        #if there is no more pages print out warning messagge and break loop
         print('No more products found!')
         break
 
